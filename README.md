63f48a0bfd9b4903ed61c54f0b68ddbd5552ccde
kjn <hiroshi.kajino.1989@gmail.com>
2021-10-14 23:35:51 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# 課題
+
+## 内容
+
+- 課題1: 税抜き価格を入力として、税込価格（消費税8%、整数でなくてもOK）を出力する関数 `assignment1` を完成させよ。
+
+- 課題2: `base` と `exponent` を入力として、 `base` の `exponent` 乗を出力する関数 `assignment2` を完成させよ。
+
+- 課題3: `x` を入力として、 (1+x) の 1/x 乗を出力する関数 `assignment3` を完成させよ。
+
+## 実行環境の作り方
+`pip3 install -r requirements.txt`
+
+## 実行コマンド
+`python test_main.py`
---
+++ main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+'''
+課題1: 税抜き価格を入力として、税込価格（消費税8%、整数でなくてもOK）を出力する関数 `assignment1` を完成させよ。
+課題2: `base` と `exponent` を入力として、 `base` の `exponent` 乗を出力する関数 `assignment2` を完成させよ。
+課題3: `x` を入力として、 (1+x) の 1/x 乗を出力する関数 `assignment3` を完成させよ。
+'''
+
+def assignment1(price):
+    pass
+
+def assignment2(base, exponent):
+    pass
+
+def assignment3(x):
+    pass
